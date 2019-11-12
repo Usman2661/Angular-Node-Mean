d59a0ac7f1c12a1f71621562523fa8b501d86693
Usman Ali <usmanusman136@hotmail.com>
2019-11-12 13:04:52 +0500

    changing to reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule , ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule, MatCardModule , MatButtonModule ,
   MatToolbarModule ,
@@ -26,6 +26,7 @@ import { AppRoutingModule } from './app-routing.module';
     MatProgressSpinnerModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     MatInputModule,
     MatToolbarModule,
     MatCardModule,
