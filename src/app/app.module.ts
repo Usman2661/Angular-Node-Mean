fb5558695ee37ed1e0d0a895396cfc4842972a06
Usman Ali <usmanusman136@hotmail.com>
2019-11-13 22:24:04 +0500

    login and signup component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,13 +14,17 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent,
     HeaderComponent,
-    PostListComponent
+    LoginComponent,
+    PostListComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
